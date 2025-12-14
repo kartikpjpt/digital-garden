143f62cd6e665403be4662b0ba5abfc85a4b77e0
Kartik Prajapat <kr.pjpt@gmail.com>
2025-12-14 17:35:10 +0530

    Update resume link

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1BZoB_d9ja6sQHadPApnwz8KqwqKTkKgA/view?usp=sharing"
+                    "https://drive.google.com/file/d/1gOv_y6AZOSYU3pPonf9UMeUewVtlOyEA/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
